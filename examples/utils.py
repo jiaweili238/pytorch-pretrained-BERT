3c2c6ce28f9c44345a2b74badc517b3dd384c95a
Jiawei <jiawei.leegc@gmail.com>
2019-03-12 22:45:24 -0500

    update utils

--- examples/utils.py
+++ examples/utils.py
@@ -1011,3 +1011,87 @@ def get_save_dir(base_dir, name, training, id_max=100):
 
     raise RuntimeError('Too many save directories created with the same name. \
                        Delete old save directories or use another name.')
+
+
+def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
+    if not ground_truths:
+        return metric_fn(prediction, '')
+    scores_for_ground_truths = []
+    for ground_truth in ground_truths:
+        score = metric_fn(prediction, ground_truth)
+        scores_for_ground_truths.append(score)
+    return max(scores_for_ground_truths)
+
+
+def eval_dicts(gold_dict, pred_dict, no_answer):
+    avna = f1 = em = total = 0
+    for key, value in pred_dict.items():
+        total += 1
+        ground_truths = gold_dict[key]['answers']
+        prediction = value
+        em += metric_max_over_ground_truths(compute_em, prediction, ground_truths)
+        f1 += metric_max_over_ground_truths(compute_f1, prediction, ground_truths)
+        if no_answer:
+            avna += compute_avna(prediction, ground_truths)
+
+    eval_dict = {'EM': 100. * em / total,
+                 'F1': 100. * f1 / total}
+
+    if no_answer:
+        eval_dict['AvNA'] = 100. * avna / total
+
+    return eval_dict
+
+
+def compute_avna(prediction, ground_truths):
+    """Compute answer vs. no-answer accuracy."""
+    return float(bool(prediction) == bool(ground_truths))
+
+
+# All methods below this line are from the official SQuAD 2.0 eval script
+# https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
+def normalize_answer(s):
+    """Convert to lowercase and remove punctuation, articles and extra whitespace."""
+
+    def remove_articles(text):
+        regex = re.compile(r'\b(a|an|the)\b', re.UNICODE)
+        return re.sub(regex, ' ', text)
+
+    def white_space_fix(text):
+        return ' '.join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def get_tokens(s):
+    if not s:
+        return []
+    return normalize_answer(s).split()
+
+
+def compute_em(a_gold, a_pred):
+    return int(normalize_answer(a_gold) == normalize_answer(a_pred))
+
+
+def compute_f1(a_gold, a_pred):
+    gold_toks = get_tokens(a_gold)
+    pred_toks = get_tokens(a_pred)
+    common = Counter(gold_toks) & Counter(pred_toks)
+    num_same = sum(common.values())
+    if len(gold_toks) == 0 or len(pred_toks) == 0:
+        # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
+        return int(gold_toks == pred_toks)
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(pred_toks)
+    recall = 1.0 * num_same / len(gold_toks)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
